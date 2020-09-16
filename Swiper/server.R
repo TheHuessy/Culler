fe301efcc71b11da8056df386f16eda4c98ab920
James Huessy <jameshuessy@gmail.com>
2020-09-15 23:52:57 -0400

    env switch

--- Swiper/server.R
+++ Swiper/server.R
@@ -1,4 +1,3 @@
-library(os)
 library(shiny)
 library(shinysense)
 library(magick)
@@ -16,7 +15,7 @@ print(the_date)
 
 ##############
 
-creds <<- read_yaml(os.environ['CREDS_PATH'])
+creds <<- read_yaml(Sys.getenv('CREDS_PATH'))
 
 sql_driver <<- dbDriver("PostgreSQL")
 
