619dada63a4bd0b272ee62b3c9213787c4af9f28
James Huessy <jameshuessy@gmail.com>
2020-12-04 13:45:13 -0500

    pre-overhaul state

--- Swiper/server.R
+++ Swiper/server.R
@@ -79,6 +79,11 @@ insta_fresh <- function(piece){
 
 
 get_link <- function(cnt){
+
+  #######
+  print(paste("Row:",work[cnt,], sep = "\n"))
+  #######
+
   test_link <- work[cnt,2]
   test <- grep(pattern="/p/", x=test_link)
   if (length(test) == 0){
@@ -91,40 +96,83 @@ get_link <- function(cnt){
 }
 
 
+#get_cnt_safe <- function(work, cnt){
+#
+#  #### Maybe have it just evaluate if the count is higher than the tot value, then do all the GET testing in get_link
+#  new_cnt <<- cnt + 1
+#
+#  if (new_cnt > nrow(work)){
+#    return(new_cnt)
+#  }
+#
+#  while (TRUE){
+#    next_ext_bool <<- file_ext(work[new_cnt,2]) %in% c("mp4", "mkv", "gif", "avi", "m4v", "m4p", "mpg")
+#    if (next_ext_bool == TRUE){
+#
+#      # Set this URL's keep value to 0
+#      work[new_cnt,3] <<- 0 
+#      tot_no <<- tot_no + 1
+#
+#      # Advance one more
+#      new_cnt <<- new_cnt + 1
+#    } else {
+#
+#      tester <<- get_link(new_cnt)
+#
+#      url_check <<- GET(tester)
+#
+#      if (url_check$status_code == 200){
+#        break
+#      } else {
+#        work[new_cnt,3] <<- 9
+#        tot_no <<- tot_no + 1
+#        new_cnt <<- new_cnt + 1
+#      }
+#    }
+#  }
+#  return(new_cnt)
+#}
+
 get_cnt_safe <- function(work, cnt){
+
+  #### Maybe have it just evaluate if the count is higher than the tot value, then do all the GET testing in get_link
   new_cnt <<- cnt + 1
 
-  if (new_cnt > nrow(work)){
+  if (new_cnt < nrow(work)){
     return(new_cnt)
+  } else {
+    pull_new_cohort()
   }
-
-  while (TRUE){
-    next_ext_bool <<- file_ext(work[new_cnt,2]) %in% c("mp4", "mkv", "gif", "avi", "m4v", "m4p", "mpg")
-    if (next_ext_bool == TRUE){
-
-      # Set this URL's keep value to 0
-      work[new_cnt,3] <<- 0 
-      tot_no <<- tot_no + 1
-
-      # Advance one more
-      new_cnt <<- new_cnt + 1
-    } else {
-
-      tester <<- get_link(new_cnt)
-
-      url_check <<- GET(tester)
-
-      if (url_check$status_code == 200){
-        break
-      } else {
-        work[new_cnt,3] <<- 9
-        tot_no <<- tot_no + 1
-        new_cnt <<- new_cnt + 1
-      }
-    }
-  }
-  return(new_cnt)
 }
+
+
+#  while (TRUE){
+#    next_ext_bool <<- file_ext(work[new_cnt,2]) %in% c("mp4", "mkv", "gif", "avi", "m4v", "m4p", "mpg")
+#    if (next_ext_bool == TRUE){
+#
+#      # Set this URL's keep value to 0
+#      work[new_cnt,3] <<- 0 
+#      tot_no <<- tot_no + 1
+#
+#      # Advance one more
+#      new_cnt <<- new_cnt + 1
+#    } else {
+#
+#      tester <<- get_link(new_cnt)
+#
+#      url_check <<- GET(tester)
+#
+#      if (url_check$status_code == 200){
+#        break
+#      } else {
+#        work[new_cnt,3] <<- 9
+#        tot_no <<- tot_no + 1
+#        new_cnt <<- new_cnt + 1
+#      }
+#    }
+#  }
+#  return(new_cnt)
+#}
 
 save_file <- function(work, cnt){
 
@@ -161,15 +209,16 @@ save_file <- function(work, cnt){
   last_saved <<- cnt-1
 }
 
-end_img <- "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Thats_all_folks.svg/1019px-Thats_all_folks.svg.png"
-
-cnt <<- get_cnt_safe(work,0)
 
 get_link <- function(cnt){
   test_link <- work[cnt,2]
 
-  ##########
-  print(paste("Test link:", test_link))
+  #########
+  print(paste("CNT:", cnt))
+  print(paste("Generated: Link:", test_link))
+  print("====================")
+  print(work[cnt,])
+  print("====================")
   ##########
   
 
@@ -188,13 +237,91 @@ get_link <- function(cnt){
   return(output_link)
 }
 
+pull_new_cohort <- function(cnt){
+  save_diff <<- cnt - last_saved
+
+  if (save_diff != 1){
+    sql_con <<- dbConnect(sql_driver,
+                          host=creds$pg_host,
+                          user=creds$pg_user,
+                          password=creds$pg_pw,
+                          dbname="strobot"
+    )
+
+    for (idx in (last_saved):(cnt-1)){
+      link <- work[idx,2]
+      new_keep <- work[idx,3]
+      tbl_name <- work[idx,4]
+      if (tbl_name == "culling_external"){
+        col_name <- "piece"
+      } else {
+        col_name <- "end_link"
+      }
+      update_string <- paste("UPDATE ",
+                             tbl_name,
+                             " SET keep = ",
+                             new_keep,
+                             " WHERE ",
+                             col_name,
+                             " = '",
+                             link,
+                             "'",
+                             sep = ""
+      )
+
+      dbExecute(sql_con, update_string)
+    }
+    dbDisconnect(sql_con)
+  }
+
+  work <<- pull_data()
+  tot_left <<- pull_total()
+
+  cnt <<- get_cnt_safe(work,0)
+
+  last_saved <<- 0
+  tot_yes <<- 0
+  tot_no <<- 0
+  yes_pct <<- ""
+  tot <<- nrow(work)
+  if (tot == 0){
+    img <- image_read(end_img) %>%
+      image_write("tmp.jpg")
+  } else{
+    img_link <<- get_link(cnt)
+    img <<- image_read(img_link) %>% 
+      image_write("tmp.jpg")
+  }
+
+}
+
+end_img <- "https://upload.wikimedia.org/wikipedia/commons/thumb/e/ea/Thats_all_folks.svg/1019px-Thats_all_folks.svg.png"
+
+cnt <<- get_cnt_safe(work,0)
 
 shinyServer(function(input, output, session) {
+              ###### APP DEPENDANT FUNCTIONS ######
+
+              save_if_5 <- function(cnt) {
+
+                if ((cnt-1) %% 5 == 0){
+                  save_file(work, cnt)
+                  output$ticker <- renderUI({
+                    h4(paste(cnt, " of ", tot, " * (", tot_left, ")", sep = ""))
+                  })
+                } else {
+                  output$ticker <- renderUI({
+                    h4(paste(cnt, " of ", tot, " (", tot_left, ")", sep = ""))
+                  })
+                }
+              }
+
+
               onStop(function(){
                        # Put this in the log so we know where a session ended
                        print("========================")
                        dbDisconnect(sql_con)
-})
+                  })
               object_swipe <- callModule(shinyswipr, "swiper_object")
 
               if (tot == 0){
@@ -237,75 +364,11 @@ shinyServer(function(input, output, session) {
 
                              yes_pct <<- round(tot_yes/cnt, digits = 3)*100
 
-                             #### SAVE IF DIVISABLE BY 5 ####
-
-                             if ((cnt-1) %% 5 == 0){
-                               save_file(work, cnt)
-                               output$ticker <- renderUI({
-                                 h4(paste(cnt, " of ", tot, " * (", tot_left, ")", sep = ""))
-                               })
-                             } else {
-                               output$ticker <- renderUI({
-                                 h4(paste(cnt, " of ", tot, " (", tot_left, ")", sep = ""))
-                               })
-                             }
-
+                             save_if_5(cnt)
 
                              if (cnt > tot){
-                               save_diff <<- cnt - last_saved
 
-                               if (save_diff != 1){
-                               sql_con <<- dbConnect(sql_driver,
-                                                     host=creds$pg_host,
-                                                     user=creds$pg_user,
-                                                     password=creds$pg_pw,
-                                                     dbname="strobot"
-                               )
-
-                                 for (idx in (last_saved):(cnt-1)){
-                                   link <- work[idx,2]
-                                   new_keep <- work[idx,3]
-                                   tbl_name <- work[idx,4]
-                                   if (tbl_name == "culling_external"){
-                                     col_name <- "piece"
-                                   } else {
-                                     col_name <- "end_link"
-                                   }
-                                   update_string <- paste("UPDATE ",
-                                                          tbl_name,
-                                                          " SET keep = ",
-                                                          new_keep,
-                                                          " WHERE ",
-                                                          col_name,
-                                                          " = '",
-                                                          link,
-                                                          "'",
-                                                          sep = ""
-                                   )
-
-                                   dbExecute(sql_con, update_string)
-                                 }
-                                 dbDisconnect(sql_con)
-                               }
-
-                               work <<- pull_data()
-                               tot_left <<- pull_total()
-
-                               cnt <<- get_cnt_safe(work,0)
-
-                               last_saved <<- 0
-                               tot_yes <<- 0
-                               tot_no <<- 0
-                               yes_pct <<- ""
-                               tot <<- nrow(work)
-                               if (tot == 0){
-                                 img <- image_read(end_img) %>%
-                                   image_write("tmp.jpg")
-                               } else{
-                                 img_link <<- get_link(cnt)
-                                 img <<- image_read(img_link) %>% 
-                                   image_write("tmp.jpg")
-                               }
+                               pull_new_cohort(cnt)
 
                              } else {
                                ## Update image and resave tmp
@@ -325,7 +388,7 @@ shinyServer(function(input, output, session) {
 
               })
 
-    })
+      })
 
 
 
